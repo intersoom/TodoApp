fa4f28f9d6147e3dee84d7b4d9cd0c686905da3e
intersoom <sallybig@naver.com>
2022-09-08 14:58:58 +0900

    항목 삭제 적용 완료

--- App.js
+++ App.js
@@ -33,6 +33,11 @@ function App() {
     );
     setTodos(nextTodos);
   };
+
+  const onRemove = id => {
+    const nextTodos = todos.filter(todo => todo.id !== id);
+    setTodos(nextTodos);
+  };
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -43,7 +48,7 @@ function App() {
           {todos.length === 0 ? (
             <Empty />
           ) : (
-            <TodoList todos={todos} onToggle={onToggle} />
+            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
           )}
           <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
